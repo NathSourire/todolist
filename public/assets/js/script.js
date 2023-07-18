9efc5caaa040b8b78279695645e03bd6d099cd68
NathSourire <nathsourire@hotmail.com>
2023-07-18 11:56:16 +0200

    Co-authored-by: Valentin <Valentin1610@users.noreply.github.com>

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,21 +1,29 @@
-let addElement = document.querySelector("#addElement")
-let element ;
-
-console.log(element);
-console.log(addElement);
+let addElement = document.querySelector('#addElement');
+let element;
+let counter = 0;
 
 if (localStorage.getItem('elements')){
-    element = JSON.parse(localStorage.getItem('elements'))
+    element = JSON.parse(localStorage.getItem('elements'));
 } else {
     element = []
 }
 
 addElement.addEventListener('click', (e) => {
-    e.preventDefault()
-    let inputEl = document.querySelector('#task').value  
-    element.push(inputEl)
-    let elementstringify = JSON.stringify(element)
-    localStorage.setItem('element', elementstringify)
+    e.preventDefault();
+    let inputEl = document.getElementById('task').value;
+    element.push(inputEl);
+    let elementstringify = JSON.stringify(element);
+    localStorage.setItem('element', elementstringify);
     console.log(elementstringify);
     task.value = ''
+
 })
+
+
+// inputEl.forEach(inputEl => {
+//     if (inputEl.id == 'task0') { 
+// inputEl.id = 'task' + counter;
+//     }
+// });
+
+
