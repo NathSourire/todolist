bfa099f40d203be405640ee3c1177882bcf90168
NathSourire <nathsourire@hotmail.com>
2023-07-19 16:54:50 +0200

    Co-authored-by: Valentin <Valentin1610@users.noreply.github.com> Co-authored-by: BihikovDev <BihikovDev@users.noreply.github.com> Co-authored-by: justinverhaeghe <justinverhaeghe@users.noreply.github.com>

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,67 +4,114 @@ let taskList = document.getElementById('list')
 let taskEl = document.getElementById('task')
 let todolist = []
 let btnResetEl = document.getElementById('btnReset')
-
+let btnFinishEl = document.getElementById('btnFinish')
 
 /*Fonction intermediare */
+// Ajout du code dans le HTML.
 const updateDom = () => {
     let taskInputText = taskEl.value
     let listItem = document.createElement("li")
     listItem.classList.add('d-flex')
-    listItem.innerHTML = `<div class="textTask form-check form-switch">
-    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">${taskInputText}</div>
-    <div class="editDeleteBtn "><button class = "btnPencilLi" type=button><i class="bi bi-pencil"></i></button><button class= 'btnTrashLi' type=button><i class="bi bi-trash"></i></button></div>`;
     listItem.id = Date.now()
+    listItem.innerHTML = `<div class= "textTask form-check form-switch" >
+    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"><span>${taskInputText}</span></div>
+    <div class= "editDeleteBtn" </button><button class= 'btnTrashLi' type=button><i class="bi bi-trash"></i></button></div>`;
     taskList.appendChild(listItem)
+    const checkbox = listItem.querySelector('.form-check-input');
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
+            moveToCompletedTasks(listItem);
+        }  else {
+            moveBackToTaskList(listItem);
+        }
+    });
     return listItem.id
-
+    listItem.document.createElement('input')
+    listItem.innerHTML = 'input'
+    
 }
 // sauvegarde
 const saveTask = (listItemId) => {
     let taskElValue = listItemId + ' ' + taskEl.value
     todolist.push(taskElValue)
     let todolistStringify = JSON.stringify(todolist)
-    localStorage.setItem('id, todolist', todolistStringify)
-    // console.log(elementstringify);  
+    localStorage.setItem('todolist', todolistStringify)
+    // console.log(elementstringify);
+    
 }
-
-
-// const deleteTask = () => {
-//     spanItemTrash.id.removeItem('listItem')
-// }
 
 const clearStorage = () => {
     localStorage.removeItem('todolist')
-    taskList.innerHTML = ''
+    taskList.innerHTML = '';
+    CompletedTaskList.innerHTML = '';
 }
 
 /* fonction principale */
 const addTask = () => {
+    if (taskEl.value === ""){
+        alert("Ajoutez une tâche s'il vous plaît !")
+        return false;
+    }
     let listItemId = updateDom()
     saveTask(listItemId)
     taskEl.value = ''
 }
+const editTask = () =>{
+    
+}
+// Fonction de restauration du local storage.
 const restoreTask = () => {
     if (localStorage.getItem('todolist')) {
-        todolist = JSON.parse(localStorage.getItem('todolist'))
+        todolist = JSON.parse(localStorage.getItem('todolist'));
+
+        todolist.forEach((taskItem) => {
+            const [listItemId, taskText] = taskItem.split(' ');
+            const listItem = document.createElement('li');
+            listItem.classList.add('d-flex');
+            listItem.id = listItemId;
+            listItem.innerHTML = `
+                <div class="textTask form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                    <span>${taskText}</span>
+                </div>
+                <div class="editDeleteBtn">
+                    <button class="btnTrashLi" type="button"><i class="bi bi-trash"></i></button>
+                </div>`;
+            taskList.appendChild(listItem);   
+            
+            const checkbox = listItem.querySelector('.form-check-input');
+            checkbox.addEventListener('change', () => {
+                if (checkbox.checked) {
+                    moveToCompletedTasks(listItem);
+                }
+                else {
+                    moveBackToTaskList(listItem);
+                }
+            });
+            listItem.addEventListener('click', editTask)
+            
+        });
     }
-}
+};
+
+const moveToCompletedTasks = (listItem) => {
+    const completedTaskList = document.getElementById('CompletedTaskList');
+    completedTaskList.appendChild(listItem);
+};
+const moveBackToTaskList = (listItem) => {
+    const taskList = document.getElementById('list');
+    taskList.appendChild(listItem);
+};
+
+// Suppression du local storagec
 const deleteAllTask = () => {
     clearStorage()
 }
+// Suppresion d'une tâche 
 
-const editTask = () => {
-    let newInputTask = document.createElement('input')
-    newInputTask.type = 'text';
-    newInputTask.name= 'nouveauInput';
-    newInputTask.placeholder ="Saisir une nouvelle tâche";
-}
+// listItem.addEventListener('click' removeTask )
 
 /* ecouteur d'evenement */
 btnAddEl.addEventListener('click', addTask)
 btnResetEl.addEventListener('click', deleteAllTask)
-// btnTrashLi.addEventListener('click', deleteTask)
 window.addEventListener('load', restoreTask)
-
-
-
